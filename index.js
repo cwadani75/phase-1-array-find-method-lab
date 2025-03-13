da74155bfb09b6e7cb38377e28d74353929616e8
cwadani75 <cwadani75@gmail.com>
2025-03-13 22:52:39 +0300

    find labs

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// code your solution here
+function superbowlWin(record) {
+    // Find the first object in the record array where the result is "W" (Win)
+    let win = record.find(game => game.result === "W");
+    
+    // If a win is found, return the year; otherwise, return undefined
+    return win ? win.year : undefined;
+}
+
+// Export the function if you're using Node.js
+module.exports = superbowlWin;
